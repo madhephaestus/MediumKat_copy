2415f38d377e3f15052fb08dcc5ea4e704ff09e0
Kevin Harrington <mad.hephaestus@gmail.com>
2020-01-03 10:24:55 -0500

    new Robot content

--- launch.groovy
+++ launch.groovy
@@ -1,4 +1,14 @@
-ScriptingEngine.gitScriptRun(	"https://github.com/OperationSmallKat/SmallKat_V2.git", 
-								"loadRobot.groovy", 
-["https://github.com/madhephaestus/MediumKat_copy.git",
-		"MediumKat_copy.xml","GameController_22","hidDevice","MediumKat_copy"]);
+//Uncommment this section to update device loader	
+// ScriptingEngine.copyGitFile("https://github.com/OperationSmallKat/SmallKat_V2.git",
+//								"https://github.com/madhephaestus/MediumKat_copy.git", 
+//								"loadRobot.groovy")
+MobileBase robot= ScriptingEngine.gitScriptRun(
+	"https://github.com/madhephaestus/MediumKat_copy.git", 
+	"loadRobot.groovy", 
+	["https://github.com/madhephaestus/MediumKat_copy.git",
+	"MediumKat_copy.xml",
+	"GameController_22",
+	"hidDevice",
+	"MediumKat_copy"]
+);
+println robot
